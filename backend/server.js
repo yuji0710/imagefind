7b9342dea38a560f5a86d413f472342f2a7bb213
yuji <142722535+yuji0710@users.noreply.github.com>
2025-05-18 20:04:19 +0530

    cv

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 app.use(router);
 // MongoDB Connection
-const MONGO_URI = process.env.MONGO_URI ||"mongodb+srv://harshm0710:harshm0710@cluster0.mongodb.net/imagefind?retryWrites=true&w=majority"; 
+const MONGO_URI = process.env.MONGO_URI ||"mongodb+srv://harshm0710:harshm0710@cluster0.mongodb.net/imagefind?retryWrites=true&w=majoritymongodb+srv://harshm0710:harshm0710@cluster0.7xqii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch(err => {
